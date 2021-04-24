979262d2e826049bc27825084bd0425c422da404
seanhk12 <skim867@aucklanduni.ac.nz>
2021-04-24 20:19:53 +1200

    pump out the bond buy/sells to every 2 messages

--- Jane-Street-ETF/src/Bot.java
+++ Jane-Street-ETF/src/Bot.java
@@ -73,7 +73,7 @@ public class Bot
                 
                 
 
-            	for(int i = 0; i<5; i++) {
+            	for(int i = 0; i<2; i++) {
                 	from_exchange.readLine();
                 	if (message[0].equals("CLOSE")) {
                         System.out.println("The round has ended");
